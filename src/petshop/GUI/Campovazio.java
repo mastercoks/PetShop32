6f6dea18e46ff139cd62a4be6a00f8e25898784b
matheus <matheus@matheus-A410-K-BE47P1>
2014-11-25 00:04:25 -0200

    [+] add Campovazio. e TelaAddCliente

---
+++ src/petshop/GUI/Campovazio.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package petshop.GUI;
+import petshop.GUI.Login;
+import petshop.GUI.TelaAddCliente;
+/**
+ *
+ * @author matheus
+ */
+public class Campovazio {
+
+    //metodo para controlar os labels lbFalta
+    public void textfield(javax.swing.JTextField txt, javax.swing.JLabel label) {
+        if (txt.getText().length() <= 0) {
+            label.setVisible(true);
+        } else {
+        }
+    }
+
+    public void formattedtextfield(javax.swing.JFormattedTextField txt, javax.swing.JLabel label) {
+//        int numero = Integer.parseInt(txt.getText());
+        String string = new String(txt.getText());
+        string = string.replace('-', ' ');
+        string = string.replace('(', ' ');
+        string = string.replace(')', ' ');
+        string = string.replace('.', ' ');
+        string = string.replace('/', ' ');
+        string = string.replaceAll(" ", " ");
+
+        if (string.substring(1, 10).trim().equals("")) {
+            label.setVisible(true);
+        } else {
+            label.setVisible(false);
+        }
+    }
+
+    public void combobox(javax.swing.JComboBox txt, javax.swing.JLabel label) {
+        int item = new Integer(txt.getSelectedIndex());
+//        String string;
+//        string = Integer.toString(txt.getSelectedIndex());
+        if (item == 0) {
+            label.setVisible(true);
+        } else {
+            label.setVisible(false);
+        }
+    }
+    
+}
